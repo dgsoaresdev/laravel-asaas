fc27231c0d42dce7e5edcc6d2ce1584b9b7cb7db
Diogo Soares <dg@diogosoares.com.br>
2024-02-25 23:53:21 -0300

    TASK 3: Instalando o Bootstrap e as dependencias do package..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,9 @@
             }
         </style>
     </head>
-    <body class="antialiased">
+    <body class="antialiased mt-5 pt-5">
+    <button type="button" class="btn btn-primary">Primary</button>
+    
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
